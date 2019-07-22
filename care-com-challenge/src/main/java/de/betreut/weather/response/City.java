39aa49aec4a42e3de3afc4e9b91d8d377a512c86
osmanyildiz81 <osman26y@gmail.com>
2019-07-22 11:14:07 +0300

    Create City.java

---
+++ care-com-challenge/src/main/java/de/betreut/weather/response/City.java
@@ -0,0 +1,15 @@
+package de.betreut.weather.response;
+
+public class City {
+
+    String cityName;
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+}
