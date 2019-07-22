5bd5cd1801ce2b5fcbc8c3d1bf5375bff10f60c7
osmanyildiz81 <osman26y@gmail.com>
2019-07-22 12:17:25 +0300

    Create TestConfig.java

---
+++ care-com-challenge/src/test/java/de/betreut/weather/config/TestConfig.java
@@ -0,0 +1,7 @@
+package de.betreut.weather.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "de.betreut.weather")
+public class TestConfig {
+}
